a5858f0d2bd4bb9889cf520b83f39b7a60236f67
Михаил Ю. Плюснин <plyusnin@iac.local>
2017-05-05 09:32:57 +0500

    add smart grid

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "style.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
+    plugins: [
+        extractSass
+    ]
+};
